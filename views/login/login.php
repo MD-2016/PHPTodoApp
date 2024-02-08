41e42b9ad9aef7b1ee3e8d4ddf3e51e0077595b2
MD <25043596+MD-2016@users.noreply.github.com>
2024-02-08 15:16:08 -0500

    added code for login page css and script for opening and closing modal

---
+++ views/login/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/style.css">
+    <script src="../../scripts/closemodal.js"></script>
+    <title>Login Page</title>
+</head>
+<body>
+
+    <!-- Button to open modal-->
+    <button onclick="document.getElementById('loginModal').style.display='block'">Login</button>
+    <div class="modal" id="loginModal">
+        <span onclick="document.getElementById('loginModal').style.display='none'" class="close" title="Close Login">&times;</span>
+   <form action="" method="post" class="modal-content animate">
+    <div class="container">
+        <label for="uname"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" id="" name="uname" required>
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required>
+
+        <button type="submit">Login</button>
+    </div>
+    <div class="btncontainer">
+        <button type="button" onclick="document.getElementById('loginModal').style.display='none'" class="cancelbtn">Cancel</button>
+    </div>
+    </form>
+    </div>
+</body>
+</html>
