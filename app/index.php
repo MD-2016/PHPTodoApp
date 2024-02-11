ff4f7c5313b1d5abe4b4758153c150e8bde08889
MD <25043596+MD-2016@users.noreply.github.com>
2024-02-11 17:39:06 -0500

    added index.php html

--- app/index.php
+++ app/index.php
@@ -1,3 +1,23 @@
 <?php
    
    require __DIR__ . "/vendor/autoload.php";
+   ?>
+
+
+   <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Start Page </title>
+</head>
+<body>
+    <div class="welcomecontainer">
+        <h1> Welcome to the Todo App! </h1>
+        <p> please click the login link to go to the login page in order to sign into your tasks page</p>
+    </div>
+    <div class="loginpagecontainer">
+            <a href="./views/login/login.php"></a>
+    </div>
+</body>
+</html>
