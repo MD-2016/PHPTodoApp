062180a3f8810febd6c69ac8c43687c77cb9acae
MD <25043596+MD-2016@users.noreply.github.com>
2024-02-18 18:28:18 -0500

    added Config class for path to database and modified SQLConnection to accomdate the change

--- app/class/dbhelper/SQLConnection.php
+++ app/class/dbhelper/SQLConnection.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace MD\dbhelper;
+use MD\dbhelper\config;
 
 
     class SQLConnection {
@@ -15,9 +16,9 @@ namespace MD\dbhelper;
           * return pdo connection to sqlite
           * @return \PDO
           */
-          public function connect() {
+          public  function connect() {
             if ($this->pdo == null) {
-                $this->pdo = new \PDO("sqlite:/todoDB.sqlite3");
+                $this->pdo = new \PDO("sqlite:" . Config::PATH_TO_SQLITE_FILE);
             }
 
             return $this->pdo;
---
+++ app/class/dbhelper/config.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MD\dbhelper;
+
+class Config {
+    /**
+     * path to sqlite file
+     */
+    const PATH_TO_SQLITE_FILE = "../../database/todoDB.sqlite3";
+}
