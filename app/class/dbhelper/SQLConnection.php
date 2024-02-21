714fc0ce6accd5f4d66847b85568c30143e67bb9
MD <25043596+MD-2016@users.noreply.github.com>
2024-02-20 21:53:23 -0500

    updated SQLConnection for adding user to database

--- app/class/dbhelper/SQLConnection.php
+++ app/class/dbhelper/SQLConnection.php
@@ -2,7 +2,7 @@
 
 namespace MD\dbhelper;
 use MD\dbhelper\config;
-
+use PDOException;
 
     class SQLConnection {
         /**
@@ -10,17 +10,54 @@ use MD\dbhelper\config;
          * @var type
          */
 
-         private $pdo;
+         private \PDO $pdo;
+
+         private const HASH_ALGO = PASSWORD_DEFAULT;
+
+         private const ENCRYPTION_COST = ['cost' => 11];
+
+         public function __construct() 
+         {
+            try {
+              $this->pdo = new \PDO("sqlite:" . Config::PATH_TO_SQLITE_FILE);
+            } catch(PDOException $e) {
+                echo "Error with loading database: " . $e;
+            }
+            
+         }
 
          /**
           * return pdo connection to sqlite
           * @return \PDO
           */
           public  function connect() {
-            if ($this->pdo == null) {
+            try{
                 $this->pdo = new \PDO("sqlite:" . Config::PATH_TO_SQLITE_FILE);
-            }
+            } catch(\PDOException $e) {
+              
+              echo "Error with loading database: " . $e;
+            } 
 
             return $this->pdo;
+          }
+
+          /**
+           * add user to the database query
+           * @return boolean 
+           */
+          public function addUserQuery($username, $password) {
+
+
+              $encryptedPass = password_hash($password, PASSWORD_DEFAULT, self::ENCRYPTION_COST);
+
+              $query = 'INSERT INTO `users` (`username`, `password`) VALUES(:username, :password)';
+              $stmt = $this->pdo->prepare($query);
+
+              $stmt->execute([
+                  ':username' => $username,
+                  ':password' => $encryptedPass
+              ]);
+
+              return $stmt;
           }
     }
