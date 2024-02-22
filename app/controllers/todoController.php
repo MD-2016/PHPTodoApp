01c532581269b331d2a158182008b915b30b30c5
MD <25043596+MD-2016@users.noreply.github.com>
2024-02-21 21:22:03 -0500

    fixed sqlite database and added a controller with functions to fill in later. Need to revise the models

---
+++ app/controllers/todoController.php
@@ -0,0 +1,27 @@
+<?php
+
+    namespace MD\controllers;
+
+    use MD\models\User;
+
+    class TodoController {
+        public function view_tasks() {
+            return; 
+        }
+
+        public function add_task() {
+            return;
+        }
+
+        public function edit_task() {
+            return;
+        }
+
+        public function update_task() {
+            return;
+        }
+
+        public function delete_task() {
+            return;
+        }
+    }
